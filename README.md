d0ef2a091c08523c4e3d16f0c44a336b926d0e7a
Arlo Phoenix <aarlo.phoenix@gmail.com>
2024-12-27 15:13:18 +0100

    Use WGSL analyzer instead of glasgow

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 This extension provides language support for the [Wgsl](https://www.w3.org/TR/WGSL/).
 
+```bash
+cargo install --git https://github.com/wgsl-analyzer/wgsl-analyzer.git wgsl_analyzer
+```
+
+~/.cargo/bin/ needs to be in PATH
+
 ## License
 
 MIT
--- src/wgsl.rs
+++ src/wgsl.rs
@@ -12,12 +12,9 @@ impl zed::Extension for WgslExtension {
         _language_server_id: &zed::LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        // https://github.com/nolanderc/glasgow
-        // TODO: add auto download support
-        // TODO: add support for other language servers
-        let wgsl_lsp_cmd = worktree.which("glasgow");
-        let path = wgsl_lsp_cmd.ok_or_else(|| "glasgow must be in your path".to_string())?;
-
+        let path = worktree
+            .which("wgsl_analyzer")
+            .ok_or_else(|| "wgsl_analyzer not in path.")?;
         Ok(zed::Command {
             command: path,
             args: vec![],
